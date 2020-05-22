95e13306435353c9272530bd3e6ffde162dec0ce
iTakeshi <itakeshi.net@gmail.com>
2020-05-22 13:51:12 +0900

    update default config

--- config.py
+++ config.py
@@ -46,13 +46,13 @@ class Config:
         self.SAVE_EVERY_EPOCHS = 1
         self.TRAIN_BATCH_SIZE = 1024
         self.TEST_BATCH_SIZE = self.TRAIN_BATCH_SIZE
-        self.TOP_K_WORDS_CONSIDERED_DURING_PREDICTION = 10
+        self.TOP_K_WORDS_CONSIDERED_DURING_PREDICTION = 3
         self.NUM_BATCHES_TO_LOG_PROGRESS = 100
         self.NUM_TRAIN_BATCHES_TO_EVALUATE = 1800
         self.READER_NUM_PARALLEL_BATCHES = 6  # cpu cores [for tf.contrib.data.map_and_batch() in the reader]
         self.SHUFFLE_BUFFER_SIZE = 10000
         self.CSV_BUFFER_SIZE = 100 * 1024 * 1024  # 100 MB
-        self.MAX_TO_KEEP = 10
+        self.MAX_TO_KEEP = 100
 
         # model hyper-params
         self.MAX_CONTEXTS = 200
